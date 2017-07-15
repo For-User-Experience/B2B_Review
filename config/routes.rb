0f4192e828bc24fb5d0324f641bc059c85b30ba5
Pineapple Dynamics <takumawatanabe82@gmail.com>
2017-07-15 18:13:18 +0900

    login little amend

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :accounts
   resources :services
   resources :companies
-  get "/lolin" => "accounts#login_form"
+  get "/login" => "accounts#login_form"
   post "/login" => "accounts#login"
   post "/logout" => "accounts#logout"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
