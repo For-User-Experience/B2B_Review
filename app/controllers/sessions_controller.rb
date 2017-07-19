3fdaacfff154ee66d0ebbf3cced7fef8861ba49d
Pineapple-Dynamics <takumawatanabe82@gmail.com>
2017-07-20 00:45:24 +0900

    session controller for login session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = Account.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      session[:account_id] = account.id
+      redirect_to account
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
