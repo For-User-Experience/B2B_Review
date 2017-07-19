449e27a567ef9fc1e21efc5b89df68ae2c3f83c4
Pineapple-Dynamics <takumawatanabe82@gmail.com>
2017-07-20 00:45:24 +0900

    session controller for login session

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :accounts
   resources :services
   resources :companies
-  get "login" => "accounts#login_form"
-  post "login" => "accounts#login"
-  post "logout" => "accounts#logout"
+  get    '/login' => 'sessions#new'
+  post   '/login' => 'sessions#create'
+  post   '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
